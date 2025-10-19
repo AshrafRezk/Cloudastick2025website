cac01415ba855afc5b73152fdfd17433742f225a
AshrafRezk <ashraf.rezkmattar@gmail.com>
2025-10-19 11:18:09 +0300

    Add Omnichannel App screenshots to carousel and update assets

--- src/pages/SalesforceApps.tsx
+++ src/pages/SalesforceApps.tsx
@@ -108,7 +108,11 @@ const SalesforceApps = () => {
       gradient: "from-cyan-500 to-blue-600",
       videoEmbed: `<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1127501430?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Cloudastick's new Add-on for you"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>`,
       category: "Communication",
-      screenshots: [],
+      screenshots: [
+        "/Assets/App%20store%20assets/Omnichannel%20App/1.jpg",
+        "/Assets/App%20store%20assets/Omnichannel%20App/2.jpg",
+        "/Assets/App%20store%20assets/Omnichannel%20App/3.jpg"
+      ],
       features: [
         {
           icon: Phone,
